f23fc2754726670dbcb4709ea639e06781bb4acc
Tomás Sossich <tomassossich77@gmail.com>
2023-12-03 13:40:03 -0300

    Update CharacterControl.cs
    +Code cleaning
    +Request for Damage and Healing

--- Assets/Scripts/Server/CharacterControl.cs
+++ Assets/Scripts/Server/CharacterControl.cs
@@ -6,12 +6,6 @@ public class CharacterControl : MonoBehaviourPunCallbacks
 {
     [SerializeField] private ServerManager _server;
     private Player _localClient;
-    private bool WKey => Input.GetKey(KeyCode.W);
-    private bool AKey => Input.GetKey(KeyCode.A);
-    private bool SKey => Input.GetKey(KeyCode.S);
-    private bool DKey => Input.GetKey(KeyCode.D);
-    private bool LeftKey => Input.GetKey(KeyCode.LeftArrow);
-    private bool RightKey => Input.GetKey(KeyCode.RightArrow);
 
     private void Awake()
     {
@@ -29,30 +23,6 @@ public class CharacterControl : MonoBehaviourPunCallbacks
     {
         _server.photonView.RPC("RequestConnect", _server.GetServer, PhotonNetwork.LocalPlayer); //??
     }
-    private void Update()
-    {
-        if (!_server.IsGameStarted) return;
-
-        //if (Input.GetKeyDown(KeyCode.Space)) _server.RPC("RequestAttack", _localClient);
-
-        if (LeftKey && RightKey) return;
-
-        if (LeftKey) _server.RPC("RequestRotation", _localClient, -1f);
-        if (RightKey) _server.RPC("RequestRotation", _localClient, 1f);
-    }
-    private void FixedUpdate()
-    {
-        if (!_server.IsGameStarted) return;
-
-        Vector3 direction = new Vector3();
-
-        if (AKey) direction += new Vector3(-1, 0, 0);
-        if (DKey) direction += new Vector3(1, 0, 0);
-        if (WKey) direction += new Vector3(0, 0, 1);
-        if (SKey) direction += new Vector3(0, 0, -1);
-
-        //_server.RPC("RequestMove", _localClient, direction);
-    }
 
     public override void OnMasterClientSwitched(Player newMasterClient)
     {
@@ -61,8 +31,8 @@ public class CharacterControl : MonoBehaviourPunCallbacks
     }
 
     public void Attack()
-    { 
-        
+    {
+        _server.RPC("RequestAttack", _localClient);
     }
 
     public void MoveUp()
@@ -93,5 +63,15 @@ public class CharacterControl : MonoBehaviourPunCallbacks
     public void RotateRight()
     {
         _server.RPC("RequestRotation", _localClient, 45f);
+    }
+
+    public void Heal()
+    {
+        _server.RPC("RequestHealing", _localClient);
+    }
+
+    public void Damage()
+    {
+        _server.RPC("RequestDamage", _localClient);
     }
 }
